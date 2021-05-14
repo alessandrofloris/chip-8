ee5b28f34f8dfb675328cba76048a1d29b49e900
Alessandro Floris <alessandrofloris@localhost.localdomain>
2021-05-14 15:13:38 +0200

    implementing opcodes

--- chip8.c
+++ chip8.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <time.h>
 
 #include "chip8.h"
-
-#define MEMORY_SIZE 4096 //chip 8 have 4096 memory locations
-#define PROGRAM_START_ADDRESS 512 //in the first 512 byte was usually used to store the 
-								  //chip-8 interpreter, so the progma will start from the address 512
 
 byte memory[MEMORY_SIZE]; //our memory
 
@@ -32,7 +29,7 @@ void loadRomInMemory(char *path) {
 	
 	//check if EOF is reached, or if ferror is set
 	if(!feof(ptr) || ferror(ptr)) {
-		printf("[Error] Read of the fail failed\n");
+		printf("[Error] Can't read the ROM\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -52,6 +49,11 @@ void initEmulator(char *path) {
 	//in memory at the address 512
 	cpu.pc = (word)PROGRAM_START_ADDRESS;
 
+	//initialization of the general purpose registers
+	for(int i=0;i<N_REGISTERS;i++) {
+		cpu.v[i] = 0;
+	}
+
 	//...
 }
 
@@ -68,8 +70,28 @@ word fetch() {
 	return opcode;
 }
 
-//decodes the opcode and call the relative function to execute it
+//knowing that in each opcode the is is the secondo 
+//most significant nibble, like NXNN, we can optain it
+byte getXFromOpcode(word opcode) {
+	byte x = (byte)opcode>>8;
+	x = (x&0x0F);
+
+	return x;
+}
+
+//knowing that in each opcode the is is the third 
+//most significant nibble, like NNYN, we can optain it
+byte getYFromOpcode(word opcode) {
+	byte y = (byte)opcode>>4;
+	y = (y&0x0F);
+
+	return y;
+}
+
+//decodes the opcode and execute it
 void decodeAndExecute(word opcode) {
+
+	word x, y; //support variables
 
 	switch(opcode>>12) {
 		case 0x0:
@@ -86,68 +108,99 @@ void decodeAndExecute(word opcode) {
 			}
 			break;
 		case 0x1: //1NNN, jumps to address NNN
-			//...
+			cpu.pc = (opcode&0x0FFF);
 			break;
 		case 0x2: //2NNN, Calls subroutine at NNN
 			//...
 			break;	
 		case 0x3: //3XNN, Skips the next instruction if VX equals NN. (Usually the next instruction is a jump to skip a code block)
-			//...
+			if(cpu.v[getXFromOpcode(opcode)] == (opcode&0x00FF)) {
+				(cpu.pc)++;
+			}
 			break;
 		case 0x4: //4XNN, Skips the next instruction if VX does not equal NN. (Usually the next instruction is a jump to skip a code block)
-			//...
+			if(cpu.v[getXFromOpcode(opcode)] != (opcode&0x00FF)) {
+				(cpu.pc)++;	
+			}
 			break;
 		case 0x5: //5XY0, Skips the next instruction if VX equals VY. (Usually the next instruction is a jump to skip a code block)
-			//...
+			if(cpu.v[getXFromOpcode(opcode)] == cpu.v[getYFromOpcode(opcode)]) {
+				(cpu.pc)++;	
+			}
 			break;
-		case 0x6: //6XNN, Sets VX to NN
-			//...
+		case 0x6: //6XNN, Sets VX to NN 
+			cpu.v[getXFromOpcode(opcode)] = (opcode&0x00FF);
 			break;
 		case 0x7: //7XNN, Adds NN to VX. (Carry flag is not changed)
-			//...
+			cpu.v[getXFromOpcode(opcode)] += (opcode&0x00FF);
 			break;
 		case 0x8:
 			switch(opcode&0x000F)  {
 				case 0x0: //8xy0, Sets VX to the value of VY.
-					//...
+					cpu.v[getXFromOpcode(opcode)] = cpu.v[getYFromOpcode(opcode)]; 
 					break;
 				case 0x1: //8XY1, Sets VX to VX or VY. (Bitwise OR operation)
-					//...
+					cpu.v[getXFromOpcode(opcode)] |= cpu.v[getYFromOpcode(opcode)]; 
 					break;
 				case 0x2: //8XY2, Sets VX to VX and VY. (Bitwise AND operation)
-					//...
+					cpu.v[getXFromOpcode(opcode)] &= cpu.v[getYFromOpcode(opcode)]; 
 					break;	
 				case 0x3: //8XY3, Sets VX to VX xor VY. 
-					//...
+					cpu.v[getXFromOpcode(opcode)] ^= cpu.v[getYFromOpcode(opcode)]; 
 					break;
 				case 0x4: //8XY4, Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there is not.
-					//...
+					x = cpu.v[getXFromOpcode(opcode)]; //i store the content of v[x] and v[y] in a 16bit var
+					y = cpu.v[getYFromOpcode(opcode)]; //so i can deduce if there is a carry from the ninth least significant bit 
+					x += y;
+
+					if(x>>8) //there's a carry
+						cpu.v[0xF] = 1;
+					else 
+						cpu.v[0xF] = 0;
+					
+					cpu.v[getXFromOpcode(opcode)] = (byte)y&0x00FF; //only store in v[y] the least significand byte 
 					break;
 				case 0x5:  //8XY5, VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there is not.
-					//...
+					cpu.v[getXFromOpcode(opcode)] = cpu.v[getXFromOpcode(opcode)] - cpu.v[getYFromOpcode(opcode)];
+					
+					if(cpu.v[getYFromOpcode(opcode)] <= cpu.v[getXFromOpcode(opcode)]) {
+						cpu.v[0xF] = 1; //no borrow
+					} else {
+						cpu.v[0xF] = 0; //borrow
+					}  
 					break;
 				case 0x6:  //8XY6, Stores the least significant bit of VX in VF and then shifts VX to the right by 1
-					//...
+					cpu.v[0xF] = (cpu.v[getXFromOpcode(opcode)]&0x0001);
+					cpu.v[getXFromOpcode(opcode)] = (cpu.v[getXFromOpcode(opcode)]>>1);
 					break;
 				case 0x7:  //8XY7, Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there is not
-					//...
+					cpu.v[getXFromOpcode(opcode)] = cpu.v[getYFromOpcode(opcode)] - cpu.v[getXFromOpcode(opcode)];
+					
+					if(cpu.v[getXFromOpcode(opcode)] <= cpu.v[getYFromOpcode(opcode)]) {
+						cpu.v[0xF] = 1; //no borrow
+					} else {
+						cpu.v[0xF] = 0; //borrow
+					}  
 					break;
 				case 0xE: //8XYE, Stores the most significant bit of VX in VF and then shifts VX to the left by 1
-					//...
+					cpu.v[0xF] = ((cpu.v[getXFromOpcode(opcode)]>>7)&0x0001);
+					cpu.v[getXFromOpcode(opcode)] = (cpu.v[getXFromOpcode(opcode)]<<1);
 					break;		
 			}
 			break;	
 		case 0x9: //9XY0, Skips the next instruction if VX does not equal VY. (Usually the next instruction is a jump to skip a code block)
-			//...
+			if(cpu.v[getXFromOpcode(opcode)] != cpu.v[getYFromOpcode(opcode)]) {
+				(cpu.pc)++;
+			}
 			break;
 		case 0xA: //ANNN, Sets I to the address NNN
-			//...
+			cpu.I = (opcode&0x0FFF);
 			break;	
 		case 0xB: //BNNN, Jumps to the address NNN plus V0
-			//...
+			cpu.pc = cpu.v[0x0]+(opcode&0x0FFF);
 			break;	
 		case 0xC: //CXNN, Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
-			//...
+			cpu.v[getXFromOpcode(opcode)] = (rand()%(255+1))&(opcode&0x00FF);
 			break;
 		case 0xD: //DXYN, Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N+1 pixels.
 				  //Each row of 8 pixels is read as bit-coded starting from memory location I; I value does not change after the execution of this instruction.
@@ -200,7 +253,7 @@ void decodeAndExecute(word opcode) {
 					break;	   		   		   				
 			}
 			break;
-	}
+		}
 }
 
 //manage the execution of the program in memory
@@ -209,7 +262,7 @@ void startEmulation() {
 
 	word opcode;
 
-	for(int i=0;i<4;i++) {
+	for(int i=0;i<100;i++) {
 		opcode = fetch(); //fetches the opcode from memory
 
 		decodeAndExecute(opcode); //decodes and executes the opcode
@@ -219,6 +272,7 @@ void startEmulation() {
 
 int main(int argc, char **argv) {
 	
+	srand(time(NULL));
 	//we expect the user to specify the path of the rom to run 
 	//when launches the emulator, like "./chip8 path/to/rom"
 	//so argc should contain the value 2, and argv[1]
--- chip8.h
+++ chip8.h
@@ -1,3 +1,10 @@
+#define N_REGISTERS 16 //chip 8 has 16 general purpose registers
+
+#define MEMORY_SIZE 4096 //chip 8 have 4096 memory locations
+
+#define PROGRAM_START_ADDRESS 512 //in the first 512 byte was usually used to store the 
+								  //chip-8 interpreter, so the progma will start from the address 512
+
 typedef unsigned char byte;
 typedef unsigned short word;
 
@@ -6,5 +13,11 @@ typedef struct {
 
 	//the PC(program counter) points to the next instruction in memory to fetch 
 	word pc;
+
+	//our cpu has 16 8bit general purpose register, from V0 to VF
+	byte v[N_REGISTERS]; 
+
+	//the 16bit address register
+	word I;
 
 }CPU;
